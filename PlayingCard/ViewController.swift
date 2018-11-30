2eaba49a926dabfb10a163525edf57b7f663699c
Tatiana Pyatykhina <tpyatyhina@mail.ru>
2018-11-30 11:38:33 +0300

    Initial Commit

---
+++ PlayingCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Татьяна Пятыхина on 30/11/2018.
+//  Copyright © 2018 Татьяна Пятыхина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
