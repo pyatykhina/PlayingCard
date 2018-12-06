47adf6e301e60a7c0a2fa3398ebf72f7258e7f82
Tatiana Pyatykhina <tpyatyhina@mail.ru>
2018-12-06 11:44:29 +0300

    init commit

---
+++ PlayingCard/PlayingCardView.swift
@@ -0,0 +1,17 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by Татьяна Пятыхина on 30/11/2018.
+//  Copyright © 2018 Татьяна Пятыхина. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+
+}
